81cf1b8c90cec634cd2479e3d9d1865457d413f0
bichpham <bichpham@users.noreply.github.com>
2019-01-19 21:48:32 -0500

    Update DependeciesInstall.sh

--- DependeciesInstall.sh
+++ DependeciesInstall.sh
@@ -1,12 +1,75 @@
-#!/bin/bash
-sudo apt-get install libasound2-dev || (echo "libasound2-dev failed"; exit 1)
-sudo apt-get install libglib2.0-dev -y || (echo "libglib2.0-dev failed"; exit 1)
-sudo apt-get install udev || (echo "udev failed"; exit 1)
-sudo apt-get install libreadline-dev -y || (echo "libreadline-dev failed"; exit 1)
-sudo apt-get install libtool -y || (echo "libtool failed"; exit 1)
-sudo apt-get install intltool -y || (echo "intltool failed"; exit 1)
-sudo apt-get install libdbus-1-dev || (echo "libdbus-1-dev failed"; exit 1)
-sudo apt-get install libudev-dev || (echo "libudev-dev failed"; exit 1)
-sudo apt-get install libical-dev -y || (echo "libical-dev failed"; exit 1)
-echo 'Completed Dependencies'
-exit 0
+#!/bin/sh
+echo "Begin script"
+sudo apt-get install libasound2-dev
+if [ $? -eq 0 ]; then 
+	echo "libasound2-dev success"
+else
+	echo "libasound2-dev failed"
+	exit 1
+fi
+
+sudo apt-get install libglib2.0-dev -y
+
+if [ $? -eq 0 ]; then 
+	echo "libglib2.0-dev success"
+else
+	echo "libglib2.0-dev failed"
+	exit 1
+fi
+
+sudo apt-get install udev
+
+if [ $? -eq 0 ]; then 
+	echo "udev success"
+else
+	echo "udev failed"
+	exit 1
+fi
+
+sudo apt-get install libreadline-dev -y
+
+if [ $? -eq 0 ]; then 
+	echo "libreadline success"
+else
+	echo "libreadline failed"
+	exit 1
+fi
+
+sudo apt-get install libtool -y
+
+if [ $? -eq 0 ]; then 
+	echo "libtool success"
+else
+	echo "libtool failed"
+	exit 1
+fi
+
+sudo apt-get install intltool -y
+
+if [ $? -eq 0 ]; then 
+	echo "intltool success"
+else
+	echo "intltool failed"
+	exit 1
+fi
+
+sudo apt-get install libdbus-1-dev
+
+if [ $? -eq 0 ]; then 
+	echo "libdbus success"
+else
+	echo "libdbus failed"
+	exit 1
+fi
+
+sudo apt-get install libical-dev -y
+
+if [ $? -eq 0 ]; then 
+	echo "libical-dev success"
+else
+	echo "libical-dev failed"
+	exit 1
+fi
+
+
+echo 'Completed Dependencies Installation'
